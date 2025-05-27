64d6081fb8643aa90045b4d2303f3e9387079fd8
samnewdich <samnewdich@gmail.com>
2025-05-27 19:01:05 +0100

    newone

---
+++ demo/vtu2/bank/script.js
@@ -0,0 +1,9 @@
+function openUserModal(){
+    document.getElementById('modaluser').style.display="block";
+    document.getElementById('modaluserinner').style.display="block";
+  }
+
+  function closeUserModal(){
+    document.getElementById('modaluser').style.display="none";
+    document.getElementById('modaluserinner').style.display="none";
+  }
